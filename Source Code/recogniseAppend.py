eeeadb3905cf6a5ac702b691071971371f74f8eb
ObaidKazi <obaidkazi03@gmail.com>
2019-03-21 00:10:25 +0530

    intergration

--- Source Code/recogniseAppend.py
+++ Source Code/recogniseAppend.py
@@ -10,68 +10,68 @@ from keras.models import load_model
 classifier = load_model('ASLModel.h5')
 
 def predictor():
-       import numpy as np
-       from keras.preprocessing import image
-       test_image = image.load_img('1.png', target_size=(64, 64))
-       test_image = image.img_to_array(test_image)
-       test_image = np.expand_dims(test_image, axis = 0)
-       result = classifier.predict(test_image)
-       
-       if result[0][0] == 1:
-              return 'A'
-       elif result[0][1] == 1:
-              return 'B'
-       elif result[0][2] == 1:
-              return 'C'
-       elif result[0][3] == 1:
-              return 'D'
-       elif result[0][4] == 1:
-              return 'E'
-       elif result[0][5] == 1:
-              return 'F'
-       elif result[0][6] == 1:
-              return 'G'
-       elif result[0][7] == 1:
-              return 'H'
-       elif result[0][8] == 1:
-              return 'I'
-       elif result[0][9] == 1:
-              return 'J'
-       elif result[0][10] == 1:
-              return 'K'
-       elif result[0][11] == 1:
-              return 'L'
-       elif result[0][12] == 1:
-              return 'M'
-       elif result[0][13] == 1:
-              return 'N'
-       elif result[0][14] == 1:
-              return 'O'
-       elif result[0][15] == 1:
-              return 'P'
-       elif result[0][16] == 1:
-              return 'Q'
-       elif result[0][17] == 1:
-              return 'R'
-       elif result[0][18] == 1:
-              return 'S'
-       elif result[0][19] == 1:
-              return 'T'
-       elif result[0][20] == 1:
-              return 'U'
-       elif result[0][21] == 1:
-              return 'V'
-       elif result[0][22] == 1:
-              return 'W'
-       elif result[0][23] == 1:
-              return 'X'
-       elif result[0][24] == 1:
-              return 'Y'
-       elif result[0][25] == 1:
-              return 'Z'
-       
+  import numpy as np
+	from keras.preprocessing import image
+	test_image = image.load_img('1.png', target_size=(64, 64))
+	test_image = image.img_to_array(test_image)
+	test_image = np.expand_dims(test_image, axis = 0)
+	result = classifier.predict(test_image)
+	
+	if result[0][0] == 1:
+		return 'A'
+	elif result[0][1] == 1:
+		return 'B'
+	elif result[0][2] == 1:
+		return 'C'
+	elif result[0][3] == 1:
+		return 'D'
+	elif result[0][4] == 1:
+		return 'E'
+	elif result[0][5] == 1:
+		return 'F'
+	elif result[0][6] == 1:
+		return 'G'
+	elif result[0][7] == 1:
+		return 'H'
+	elif result[0][8] == 1:
+		return 'I'
+	elif result[0][9] == 1:
+		return 'J'
+	elif result[0][10] == 1:
+		return 'K'
+	elif result[0][11] == 1:
+		return 'L'
+	elif result[0][12] == 1:
+		return 'M'
+	elif result[0][13] == 1:
+		return 'N'
+	elif result[0][14] == 1:
+		return 'O'
+	elif result[0][15] == 1:
+		return 'P'
+	elif result[0][16] == 1:
+		return 'Q'
+	elif result[0][17] == 1:
+		return 'R'
+	elif result[0][18] == 1:
+		return 'S'
+	elif result[0][19] == 1:
+		return 'T'
+	elif result[0][20] == 1:
+		return 'U'
+	elif result[0][21] == 1:
+		return 'V'
+	elif result[0][22] == 1:
+		return 'W'
+	elif result[0][23] == 1:
+		return 'X'
+	elif result[0][24] == 1:
+		return 'Y'
+	elif result[0][25] == 1:
+		return 'Z'
+	
 
-       
+	
 
 cam = cv2.VideoCapture(0)
 
@@ -131,27 +131,27 @@ while True:
     cv2.imwrite(img_name, save_img)
     img_text=predictor()
     if cv2.waitKey(1) == ord('c'):
-      try:
+			try:
         append_text+=img_text
-      except:
+			except:
         append_text+=''
-      cv2.putText(img1, append_text, (1, 10), cv2.FONT_HERSHEY_TRIPLEX, 0.5, (0, 255, 0))
-      try:
+			cv2.putText(img1, append_text, (1, 10), cv2.FONT_HERSHEY_TRIPLEX, 0.5, (0, 255, 0))
+			try:
         y=0
         for x in range(len(finalBuffer)):
           cv2.putText(img2, finalBuffer[x], (1, 10+y), cv2.FONT_HERSHEY_TRIPLEX, 0.5, (0, 255, 0))
           y+=20
-      except:
-        pass
-      cv2.imshow("Freezing Window", img1)     
-      cv2.imshow("Output Window", img2)
-      
-      if(len(append_text)>25):
-        finalBuffer.append(append_text)
-        print(finalBuffer[z])
-        append_text=''
-        z+=1
-        
+			except:
+	 pass
+			cv2.imshow("Freezing Window", img1)     
+			cv2.imshow("Output Window", img2)
+			
+			if(len(append_text)>25):
+	 finalBuffer.append(append_text)
+	 print(finalBuffer[z])
+	 append_text=''
+	 z+=1
+	 
 
     if cv2.waitKey(1) == 27:
     	f=open("temp.txt","w")
