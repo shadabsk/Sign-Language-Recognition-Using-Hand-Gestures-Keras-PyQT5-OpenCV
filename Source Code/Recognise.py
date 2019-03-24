82d0473bc4b2e3ee28664405641305b13775963e
shadabsk <shaikhshadabali2@gmail.com>
2019-03-25 01:08:35 +0530

    custom gesture module and stable append sentence formation

--- Source Code/Recognise.py
+++ Source Code/Recognise.py
@@ -9,6 +9,39 @@ image_x, image_y = 64,64
 from keras.models import load_model
 classifier = load_model('ASLModel.h5')
 
+#original = cv2.imread("5.png")
+
+def imgprocessing():
+  image_to_compare = cv2.imread("SampleGest/0.png")
+  original = cv2.imread("1.png")
+  sift = cv2.xfeatures2d.SIFT_create()
+  kp_1, desc_1 = sift.detectAndCompute(original, None)
+  kp_2, desc_2 = sift.detectAndCompute(image_to_compare, None)
+   
+  index_params = dict(algorithm=0, trees=5)
+  search_params = dict()
+  flann = cv2.FlannBasedMatcher(index_params, search_params)
+    
+  matches = flann.knnMatch(desc_1, desc_2, k=2)
+   
+  good_points = []
+  ratio = 0.6
+  for m, n in matches:
+      if m.distance < ratio*n.distance:
+          good_points.append(m)
+  #print(len(good_points))
+  fin=len(kp_1) -len(kp_2)
+  print(abs(fin))
+  if(abs(fin)<=2):
+    return 'space'
+    
+  result = cv2.drawMatches(original, kp_1, image_to_compare, kp_2, good_points, None)
+   
+  #cv2.imshow("result", result)
+  #cv2.imshow("Original", original)
+  #cv2.imshow("Duplicate", image_to_compare)
+  #cv2.destroyAllWindows()
+
 def predictor():
        import numpy as np
        from keras.preprocessing import image
@@ -16,8 +49,32 @@ def predictor():
        test_image = image.img_to_array(test_image)
        test_image = np.expand_dims(test_image, axis = 0)
        result = classifier.predict(test_image)
+
+       image_to_compare = cv2.imread("0.png")
+       original = cv2.imread("1.png")
+       sift = cv2.xfeatures2d.SIFT_create()
+       kp_1, desc_1 = sift.detectAndCompute(original, None)
+       kp_2, desc_2 = sift.detectAndCompute(image_to_compare, None)
        
-       if result[0][0] == 1:
+       index_params = dict(algorithm=0, trees=5)
+       search_params = dict()
+       flann = cv2.FlannBasedMatcher(index_params, search_params)
+        
+       matches = flann.knnMatch(desc_1, desc_2, k=2)
+       
+       good_points = []
+       ratio = 0.6
+       for m, n in matches:
+              if m.distance < ratio*n.distance:
+                     good_points.append(m)
+
+       if(abs(len(good_points)+len(matches))>20):
+        return 'space'
+       #print(good_points)
+       #if(abs(fin)<=2):
+             # return 'space'
+       
+       elif result[0][0] == 1:
               return 'A'
        elif result[0][1] == 1:
               return 'B'
@@ -77,8 +134,6 @@ cam = cv2.VideoCapture(0)
 
 cv2.namedWindow("Trackbars")
 
-cv2.moveWindow("Trackbars",700,30)
-
 cv2.createTrackbar("L - H", "Trackbars", 0, 179, nothing)
 cv2.createTrackbar("L - S", "Trackbars", 0, 255, nothing)
 cv2.createTrackbar("L - V", "Trackbars", 0, 255, nothing)
@@ -86,10 +141,11 @@ cv2.createTrackbar("U - H", "Trackbars", 179, 179, nothing)
 cv2.createTrackbar("U - S", "Trackbars", 255, 255, nothing)
 cv2.createTrackbar("U - V", "Trackbars", 255, 255, nothing)
 
-cv2.namedWindow("ASL Scanning Window")
+cv2.namedWindow("ASL Recognition")
 
-    
 img_text = ''
+img_text1 = ''
+#z5=1
 while True:
     ret, frame = cam.read()
     frame = cv2.flip(frame,1)
@@ -108,24 +164,23 @@ while True:
     imcrop = img[102:298, 427:623]
     hsv = cv2.cvtColor(imcrop, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, lower_blue, upper_blue)
-    cv2.imshow("ASL Scanning Window", frame)
+    
+    cv2.putText(frame, img_text, (30, 400), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (0, 255, 0))
+    cv2.putText(frame, img_text1, (80, 400), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (0, 255, 0))
+    cv2.imshow("ASL Recognition", frame)
     cv2.imshow("mask", mask)
-    cv2.moveWindow("mask", 1050,30)
-    
-    img1 = np.zeros((400,400,4), np.uint8)
-    #if cv2.waitKey(1) == ord('c'):
-    cv2.putText(img1, img_text, (160, 220), cv2.FONT_HERSHEY_TRIPLEX, 3.5, (0, 255, 0))
-    cv2.imshow("Output Window", img1)     
-    cv2.moveWindow("Output Window", 800,300)
-    
+        
     img_name = "1.png"
     save_img = cv2.resize(mask, (image_x, image_y))
     cv2.imwrite(img_name, save_img)
     img_text = predictor()
-        
 
     if cv2.waitKey(1) == 27:
         break
+
+   # img_text1 = imgprocessing()
+
+   
 
 
 cam.release()
